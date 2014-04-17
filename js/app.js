0be3a6d44352c556f8e08b0fe39c0d91915f2dab
Sparklebunny Stevens <jeribeth@gmail.com>
2014-04-16 20:23:29 -0600

    Moved from hot-or-cold-starter
    gh-pages wasn’t working so created new repository

---
+++ js/app.js
@@ -0,0 +1,69 @@
+
+$(document).ready(function(){
+
+	/*--- Display information modal box ---*/
+  	$(".what").click(function(){
+    	$(".overlay").fadeIn(1000);
+
+  	});
+
+  	/*--- Hide information modal box ---*/
+  	$("a.close").click(function(){
+  		$(".overlay").fadeOut(1000);
+  	});
+
+// ---------------------------------------------------
+	// List of Variables
+	var answer = Math.floor(Math.random()*10)+1;
+		console.log("The answer is " + answer);
+	var numberOfGuesses = 0;
+	var distance = null;
+
+	
+    $(".new").click(function (event) {
+        answer = Math.floor((Math.random() * 100) + 1);
+        	console.log(answer);
+		numberOfGuesses = 0;
+        distance = null;
+        $("#userGuess").val(''); 
+        numberOfGuesses = 0
+        $("#count").html(numberOfGuesses);
+        $("#guessList").html('');
+
+    });
+	
+
+	$("#guessButton").click(processGuess); 
+		function processGuess() {
+			var guess = $("#userGuess").val();
+			var numberGuess = parseInt(guess, 10);
+			distance = Math.abs(numberGuess - answer);
+
+			if (distance > 0 && distance <= 10) {
+				alert("You're getting hot. Try again.")
+			}
+			else if (distance >= 11 && distance <=20) {
+				alert("You're getting warm. Try again.")
+			}
+			else if (distance >=21 && distance <=30) {
+				alert("You're getting cold. Try again.")
+			}
+			else if (distance >=31) {
+				alert("You've landed in Antartica! Send for help.")
+			}
+			else if (distance == 0) {
+				alert("YOU'RE CORRECT! Way to go! Choose NEW GAME to play again.")
+			}
+			$("#count").html(++numberOfGuesses);
+			$("#guessList").append('<p>' + guess + '</p>');
+	}
+	
+	$("#userGuess").keypress(function(event) {
+		if (event.keyCode === 13) {
+			processGuess();
+		}
+	});
+
+});
+
+
